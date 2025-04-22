13886b1e5961d18c55a2eee4d98c4b05f9bf8bc7
juess123 <93591744+juess123@users.noreply.github.com>
2025-04-22 15:45:32 +0800

    Update strcmp.c
    push

--- CProgramMaster/strcmp.c
+++ CProgramMaster/strcmp.c
@@ -1,4 +1,5 @@
-int strcmp (const char *p1, const char *p2)
+#include <stdio.h>
+int MyStrcmp (const char *p1, const char *p2)
 {
   const unsigned char *s1 = (const unsigned char *) p1;
   const unsigned char *s2 = (const unsigned char *) p2;
@@ -15,3 +16,12 @@ int strcmp (const char *p1, const char *p2)
 
   return c1 - c2;
 }
+int main()
+{
+  char* p1="hello";
+  char* p2="hello";
+  int a=MyStrcmp(p1,p2);
+  printf("%d\n",a);
+  return 0;
+}
+//没啥好说的的  依次比较两个字符 25-4-22
